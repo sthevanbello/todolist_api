d33322e8bb3f23f396bb007c966c0c9fd76f7fc0
sthevanbello <sthevan.alves@gmail.com>
2023-04-11 11:26:46 -0300

    Configuração do swagger em ambiente de produção

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ namespace TodoList
             }
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
